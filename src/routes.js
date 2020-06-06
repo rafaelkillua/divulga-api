22cc576cb347611b7ed41e55b12e563dbcc14bb3
Rafael Sampaio Tavares <rafael_killua@msn.com>
2020-06-05 23:41:42 -0300

    is online route /

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const businessController = require('./controllers/business')
 
 module.exports = app => {
+  app.get(`/`, (_, res) => res.status(200).send('Online'))
+
   app.post(`/business`, businessController.create)
 }
